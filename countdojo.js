3f6a7e802b913fa7a936d22ecd06d909ce0b1017
vidisha-2 <42693205+vidisha-2@users.noreply.github.com>
2018-09-04 19:29:22 -0700

    Add files via upload

---
+++ countdojo.js
@@ -0,0 +1,19 @@
+for(var i = 1; i<= 100; i++)
+{	
+	var a = i%5, b = i%10;
+	//console.log(i);
+	if(a==0 && b != 0)
+	{	
+		//console.log(i);
+		console.log("coding");
+	
+	}
+	
+	if(b==0)		
+	{
+		//console.log(i);
+		console.log("coding dojo");
+	
+	}
+	
+}
